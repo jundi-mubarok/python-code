ce738cd85d86623b7a71377594b1cf52e6eb808e
Jundi Mubarok <jundimubarok74@gmail.com>
2021-10-31 06:50:29 +0700

    belajar pertama

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ Semua sintaksis dasar pemrograman terdiri dari
 """
 
 # Sekuensial
-
+"""
 print('Ibu berkata, "Pergilah ke toko"')
 print('anak menjawab, "apa yang harus saya lakukan?"')
 print('Ibu berkata lagi "Belilah 6 botol susu, jika ada telor belilah 6"')
 print("Maka budi berangkat ke toko")
 print("dan mulai berbelanja")
+"""
 
 # Percabangan
+jumlah_botol_susu = 1
+
+if jumlah_botol_susu > 0:
+    print("budi mengecek harganya dan cukup")
+    print("budi membeli botol susu")
+else :
+    print("budi tidak jadi membeli botol susu")
+
+print("budi pulang kerumah")
+print("menyampaikan hasilnya ke ibu")
