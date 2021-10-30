eb56486eab2b805f9194328ccd6c0b534fc3fcd2
Jundi Mubarok <jundimubarok74@gmail.com>
2021-10-30 12:00:47 +0700

    belajar pertama

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 """
-Ini adalah demo pertama projek dengan python
+Semua sintaksis dasar pemrograman terdiri dari
+1. sekuesnsial : langkah berurutan
+2. percabangan : langkah melompat jika kondisi terpenuhi
+3. perulangan : mengulang langkah yang sama berkali-kali sampai kondisi terpenuhi
 """
-print ("Hello World")
-print ("Hello my name is jundi")
+
+# Sekuensial
+
+print('Ibu berkata, "Pergilah ke toko"')
+print('anak menjawab, "apa yang harus saya lakukan?"')
+print('Ibu berkata lagi "Belilah 6 botol susu, jika ada telor belilah 6"')
+print("Maka budi berangkat ke toko")
+print("dan mulai berbelanja")
+
+# Percabangan
